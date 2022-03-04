84523299727c7bcab622a889029997deba9b5ec8
sbl1996 <sbl1996@126.com>
2022-03-04 21:21:53 +0800

    update REMOTE_DDIR

--- examples/voc_seg.py
+++ examples/voc_seg.py
@@ -5,7 +5,7 @@ sheet_id = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
 access_token = "aa8f2297d25b4dc6fd3d98411eb3ba53823c4f42"
 secret_file = "credentials.json"
 token_file = "token.pickle"
-envs = {"REMOTE_DDIR": "gs://tensorflow-datasets/VOC2012Segmentation"}
+envs = {"REMOTE_DDIR": "gs://tensorflow-datasets/tfds"}
 
 project = VOCSeg("VOC-Segmentation", git_repo, sheet_id, access_token, secret_file, token_file,
                  sub_sheet="VOC", worker_id=1, **envs)
